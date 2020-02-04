fe595bad8b783460991480bf389c0b785b0b1382
Athanasia Tranou <athanasiatranou@gmail.com>
2020-02-04 16:31:56 +0200

    delete comments

--- src/main/java/gr/codehub/College/controller/CollegeController.java
+++ src/main/java/gr/codehub/College/controller/CollegeController.java
@@ -3,7 +3,6 @@ package gr.codehub.College.controller;
 import gr.codehub.College.model.Course;
 import gr.codehub.College.service.Service;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileNotFoundException;
